30f2d3cd447eb8beb8db9ef20f56e0c750a4d9b0
SebastianRm2102 <juansebastianrachemillan@gmail.com>
2026-05-10 18:34:57 -0500

    Update ArbolPasajeros.java

--- src/estructuras/ArbolPasajeros.java
+++ src/estructuras/ArbolPasajeros.java
@@ -1,13 +1,17 @@
+package estructuras;
+
+import modelos.Pasajeros;
+
 class Nodo {
 
-    int ticket;
+    Pasajeros pasajero;
 
     Nodo izquierdo;
     Nodo derecho;
 
-    public Nodo(int ticket) {
+    public Nodo(Pasajeros pasajero) {
 
-        this.ticket = ticket;
+        this.pasajero = pasajero;
     }
 }
 
@@ -15,65 +19,111 @@ public class ArbolPasajeros {
 
     Nodo raiz;
 
-    public void insertar(int ticket) {
+    // INSERTAR
+    public void insertar(Pasajeros pasajero) {
 
-        raiz = insertarRecursivo(raiz, ticket);
+        raiz = insertarRecursivo(raiz, pasajero);
     }
 
-    private Nodo insertarRecursivo(Nodo actual, int ticket) {
+    private Nodo insertarRecursivo(
+            Nodo actual,
+            Pasajeros pasajero) {
 
         if (actual == null) {
 
-            return new Nodo(ticket);
+            return new Nodo(pasajero);
         }
 
-        if (ticket < actual.ticket) {
+        if (pasajero.getTicket()
+                < actual.pasajero.getTicket()) {
 
-            actual.izquierdo = insertarRecursivo(actual.izquierdo, ticket);
+            actual.izquierdo =
+                    insertarRecursivo(
+                            actual.izquierdo,
+                            pasajero);
 
-        } else if (ticket > actual.ticket) {
+        } else if (pasajero.getTicket()
+                > actual.pasajero.getTicket()) {
 
-            actual.derecho = insertarRecursivo(actual.derecho, ticket);
+            actual.derecho =
+                    insertarRecursivo(
+                            actual.derecho,
+                            pasajero);
         }
 
         return actual;
     }
 
+    // BUSCAR
     public boolean buscar(int ticket) {
 
-        boolean encontrado = buscarRecursivo(raiz, ticket);
+        boolean encontrado =
+                buscarRecursivo(raiz, ticket);
 
         if (encontrado) {
 
-            System.out.println("Pasajero encontrado con ticket: " + ticket);
-
+            System.out.println(
+                    "Pasajero encontrado");
         } else {
 
-            System.out.println("Pasajero NO encontrado");
+            System.out.println(
+                    "Pasajero NO encontrado");
         }
 
         return encontrado;
     }
 
-    private boolean buscarRecursivo(Nodo actual, int ticket) {
+    private boolean buscarRecursivo(
+            Nodo actual,
+            int ticket) {
 
         if (actual == null) {
 
             return false;
         }
 
-        if (ticket == actual.ticket) {
+        if (ticket ==
+                actual.pasajero.getTicket()) {
 
             return true;
         }
 
-        if (ticket < actual.ticket) {
+        if (ticket
+                < actual.pasajero.getTicket()) {
 
-            return buscarRecursivo(actual.izquierdo, ticket);
+            return buscarRecursivo(
+                    actual.izquierdo,
+                    ticket);
 
         } else {
 
-            return buscarRecursivo(actual.derecho, ticket);
+            return buscarRecursivo(
+                    actual.derecho,
+                    ticket);
+        }
+    }
+
+    // MOSTRAR INORDEN
+    public void mostrarInOrden() {
+
+        System.out.println(
+                "\n=== PASAJEROS ===");
+
+        mostrarInOrdenRecursivo(raiz);
+    }
+
+    private void mostrarInOrdenRecursivo(
+            Nodo actual) {
+
+        if (actual != null) {
+
+            mostrarInOrdenRecursivo(
+                    actual.izquierdo);
+
+            actual.pasajero.mostrarInfo();
+
+            mostrarInOrdenRecursivo(
+                    actual.derecho);
         }
     }
 }
