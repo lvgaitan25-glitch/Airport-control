3e9f65b9e3713b62bc22840fe7027e1caea34750
SebastianRm2102 <juansebastianrachemillan@gmail.com>
2026-05-10 19:00:02 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        VentanaPrincipal ventana = new VentanaPrincipal();
+        VentanaPrincipal ventana =
+                new VentanaPrincipal();
 
         ventana.setVisible(true);
     }
